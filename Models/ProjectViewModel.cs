c8d5732ea2b8058389c0015fc99b4efc09db059b
“Luiz-om” <luizfernando_martins@hotmail.com>
2024-07-23 22:27:34 -0400

    feat - ProjectViewModel end create methods

--- Models/ProjectViewModel.cs
+++ Models/ProjectViewModel.cs
@@ -2,11 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevFreela.API.Entities;
 
 namespace DevFreela.API.Models
 {
     public class ProjectViewModel
     {
+        public ProjectViewModel(int id, string title, string description, int idCliente, int idFreelancer, 
+        string clientName, string freelancerName, decimal totalCost, List<ProjectComment> comments)
+        {
+            this.id = id;
+            Title = title;
+            Description = description;
+            IdCliente = idCliente;
+            IdFreelancer = idFreelancer;
+            ClientName = clientName;
+            FreelancerName = freelancerName;
+            TotalCost = totalCost;
+            Comments = comments.Select(c => c.Content).ToList();
+        }
+
         public int id { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
@@ -16,6 +31,11 @@ namespace DevFreela.API.Models
         public string FreelancerName { get; set; }
         public decimal TotalCost { get; private set; }
         public List<string> Comments { get; private set; }
+
+
+        public static ProjectViewModel FromEntity(Project entity)
+        => new ProjectViewModel(entity.Id, entity.Title,entity.Description, entity.IdClient, entity.IdFreelancer,
+         entity.Client.FullName,entity.Freelancer.FullName, entity.TotalCost, entity.Comments );
 
     }
 }
