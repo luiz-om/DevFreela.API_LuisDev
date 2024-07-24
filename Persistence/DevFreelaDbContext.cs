128733bea7e17ac9067b5c0cc2cf64cc19cf8a03
“Luiz-om” <luizfernando_martins@hotmail.com>
2024-07-23 23:50:26 -0400

    feat

--- Persistence/DevFreelaDbContext.cs
+++ Persistence/DevFreelaDbContext.cs
@@ -17,6 +17,18 @@ namespace DevFreela.API.Persistence
 
         public DbSet<Project> Projects { get; set; }
         public DbSet<User> Users { get; set; }
-    
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Skill>(e =>
+                {
+                    e.HasKey(s => s.Id);
+                });
+            base.OnModelCreating(builder);
+        }
     }
 }
